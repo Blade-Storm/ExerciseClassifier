6f1310bee80dc36d4a7ac79455b327fe18a676c8
Blade Dyer <blade.dyer@gmail.com>
2019-06-18 17:46:06 -0700

    updated images used for training and testing

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ parser.add_argument('checkpoint',
                     help="The relative path to the neural network checkpoint as a pth file.")
 parser.add_argument('--top_k', default=1, type=int,
                     help="The amount of most likley classes to return for the predictions")
-parser.add_argument('--category_names', default = './cat_to_name.json',
+parser.add_argument('--category_names', default = './exercise_to_name.json',
                     help="The json file (including file path) to load category names")
 parser.add_argument('--gpu', default=False, action='store_true',
                     help="Boolean to infer with the gpu")
